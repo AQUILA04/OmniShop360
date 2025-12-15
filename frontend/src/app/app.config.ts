be4d18800b24f86a0f645ce5c60e3033a825bf17
Francis AHONSOU <45388225+AQUILA04@users.noreply.github.com>
2025-12-15 15:34:38 +0400

    frontend setup update

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,6 +2,7 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
+import { AuthConfig, provideOAuthClient } from 'angular-oauth2-oidc';
 
 import { routes } from './app.routes';
 
@@ -9,6 +10,18 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideAnimations(),
-    provideHttpClient()
+    provideHttpClient(),
+    provideOAuthClient(),
+    {
+      provide: AuthConfig,
+      useValue: {
+        issuer: 'http://localhost:8080/realms/omnishop360',
+        redirectUri: window.location.origin + '/dashboard',
+        clientId: 'omnishop-frontend',
+        responseType: 'code',
+        scope: 'openid profile email offline_access',
+        showDebugInformation: true,
+      }
+    }
   ]
 };
